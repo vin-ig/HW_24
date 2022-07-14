baeedb88fdbcd6e8a549b0e688475bff529fd777
Harry <vin-ig@yandex.ru>
2022-07-14 20:09:30 +0300

    Typing completed

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from exception import RequestError
 import os
-from typing import Iterable
+from typing import Iterable, List, Union
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
@@ -8,11 +8,9 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 def filter(data: Iterable, value: str) -> Iterable:
 	return (line.strip() for line in data if value in line)
-	# Выглядит красивее, но уходит в рекурсию
-	# return filter(lambda row: value in row, data)
 
 
-def map(data, value):
+def map(data: Iterable, value: str) -> Iterable:
 	try:
 		i = int(value)
 		return (line.split()[i] for line in data if i < len(line.split()))
@@ -20,17 +18,17 @@ def map(data, value):
 		raise RequestError('Check value')
 
 
-def unique(data, value):
+def unique(data: Iterable) -> set:
 	return set([row for row in data])
 
 
-def sort(data, value):
+def sort(data: Iterable, value: str) -> List[str]:
 	if value not in ('asc', 'desc'):
 		raise RequestError('Check value')
 	return sorted([i for i in data], reverse=False if value == 'asc' else True)
 
 
-def limit(data, value):
+def limit(data: Iterable, value: str) -> Iterable:
 	gen = iter(data)
 	for i in range(int(value)):
 		try:
@@ -39,7 +37,7 @@ def limit(data, value):
 			break
 
 
-def read(filename):
+def read(filename: str) -> Iterable:
 	with open(f'{DATA_DIR}/{filename}', 'r') as file:
 		while True:
 			try:
@@ -48,13 +46,13 @@ def read(filename):
 				break
 
 
-def get_query(data, cmd, value):
+def get_query(data: Iterable, cmd: str, value: str) -> Union[Iterable, set, List[str]]:
 	if cmd =='filter':
 		return filter(data, value)
 	elif cmd == 'map':
 		return map(data, value)
 	elif cmd == 'unique':
-		return unique(data, value)
+		return unique(data)
 	elif cmd == 'sort':
 		return sort(data, value)
 	elif cmd == 'limit':
